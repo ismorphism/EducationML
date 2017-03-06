411c6f1e79b65a33782d9d3ce42a9b47026d3f5f
bpakilla <pakillaboris@gmail.com>
2017-03-06 18:30:57 +0700

    Update

--- MLalgorithms.py
+++ MLalgorithms.py
@@ -1,42 +1,52 @@
 import numpy as np
 from numpy import exp as exp
 import matplotlib.pyplot as plt
+from scipy import special as sp
+from decimal import *
+# getcontext().prec = 25
 
-
-
-def sigm(X, k):
-    y = np.asarray([1/(1 + exp(-k*float(i)))for i in X])
+def sigm(X, k=1000):
+    y = [float((1/(1 + exp(-k*i)))) for i in X]
     return y
 
 
 class LinearClassifier:
 
-    def __init__(self):
-        self.weigths = np.zeros((5,1))
+    def __init__(self, y, X):
+        self.weigths = np.zeros((len(y), len(X)))
         self.error = []
 
     def train(self, X, y):
-        self.weigths = np.zeros(np.shape(X[0, :]))
         y_t = np.dot(X, np.transpose(self.weigths))
-        self.error.append(float(0.5*np.dot(y - y_t, y - y_t)))
-        alpha = 0.001
+        y_t = sigm(y_t)
+        self.error.append((y - y_t))
+        alpha = 0.1
         i = 0
-        max_iter = 100
-        while i < max_iter-1:
-            print(self.error[i])
+        while sum([abs(j) for j in self.error[i]]) > 0:
+            # print(self.error[i])
             i += 1
-            self.weigths += alpha*2*self.error[i - 1]*X
+            for j in range(len(y)):
+                self.weigths[j, :] += alpha*2*np.dot(sum([i[j] for i in self.error]), X)
             y_t = np.dot(X, np.transpose(self.weigths))
-            self.error.append(float(0.5 * np.dot(y - y_t, y - y_t)))
-        print(y_t, ' ', self.error[max_iter - 1])
-        x = np.arange(0, max_iter, 1)
+            y_t = sigm(y_t)
+            self.error.append((y - y_t))
+        # print(y_t, ' ', [sum(j) for j in self.error])
+        self.output = y_t
+        self.error = []
+        x = np.arange(0, i + 1, 1)
         g = np.asarray(self.error)
 
-        plt.setp(plt.plot(x, np.asarray(self.error)), color='b', linewidth=3.0)
-        plt.show()
+        # plt.setp(plt.plot(x, np.asarray([sum(j) for j in self.error])), color='b', linewidth=3.0)
+        # plt.show()
+
+    def show(self):
+        print(self.weigths)
 
     def predict(self, X_test):
-        y_test = np.matmul(X_test, np.transpose(self.weigths))
-        print('Prediction is ', y_test)
+        y_p = np.dot(X_test, np.transpose(self.weigths))
+        y_p = sigm(y_p)
+        # print('Prediction is ', y_p)
+        return y_p
 
-
+# print(sigm([10, 50, 5, 65, 99], 1))
+# print(np.shape(np.matrix([1,2,3]))[1])
--- MLtest.py
+++ MLtest.py
@@ -1,11 +1,54 @@
 import MLalgorithms as ml
 import numpy as np
+import pickle as cPickle
+import matplotlib.pyplot as plt
+from PIL import Image
 
+def unpickle(file):
+    fo = open(file, 'rb')
+    dict = cPickle.load(fo, encoding='latin')
+    fo.close()
+    return dict
 
-X = np.matrix([1.1, 2.2, 3.3, 1, 7])
-y = np.matrix(3.5)
-clf = ml.LinearClassifier()
-# clf.show()
-clf.train(X, y)
-# clf.show()
-clf.predict([11, 2.2, 3.3, 10, 7])
+data = unpickle("cifar-10-batches-py/data_batch_1")
+label_names = unpickle("cifar-10-batches-py/batches.meta")
+print(data.keys())
+print(label_names['label_names'])
+dict = label_names['label_names']
+X = data['data'][0, :]
+y = np.zeros((10,))
+y[data['labels'][0]] = 1
+
+clf = ml.LinearClassifier(y, X)
+plt.ion()
+
+for i in range(10000):
+    X = data['data'][i, :]
+    y = np.zeros((10,))
+    y[data['labels'][i]] = 1
+    clf.train(X, y)
+    # print(i)
+    number = clf.output.index(1)
+    print(dict[number])
+    # img = Image.fromarray(X.reshape((32, 32, 3)), 'RGB')
+    # img.save('my.png')
+    # img.show()
+    # plt.imshow(X.reshape((32, 32, 3)))
+    # plt.show()
+    # plt.pause(2)
+
+print(clf.weigths)
+
+total_err = 0
+for i in range(100, 200):
+    y = np.zeros((10,))
+    y[data['labels'][i]] = 1
+    X_test = data['data'][i, :]
+    pred = clf.predict(X_test)
+    try:
+        pred_number = pred.index(1)
+        print('True is ', dict[data['labels'][i]], 'Prediction is ', dict[pred_number])
+    except ValueError:
+        pass
+    total_err += sum([abs(i) for i in (clf.predict(X_test) - y)])
+print(total_err)
